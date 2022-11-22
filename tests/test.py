7d80176760fd3c1d51133ebdb84cde092a6432ea
Muhamad Aldy B <muhamad.aldy_bintang.lw2@is.naist.jp>
2022-11-22 16:38:46 +0900

    update test.py

--- tests/test.py
+++ tests/test.py
@@ -4,6 +4,6 @@ from speciallecture.CSVPrinter import CSVPrinter
 
 class TestCSVPrinter(unittest.TestCase):
     def test_read(self):
-        printer = CSVPrinter("sample.csv")
+        printer = CSVPrinter("./sample.csv")
         l = printer.read()
         self.assertEqual(6, len(l))
